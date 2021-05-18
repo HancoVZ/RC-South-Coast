bae0bb4fad866b08ed4ddb97d9960a1849af83db
HancoVZ <55700460+HancoVZ@users.noreply.github.com>
2021-05-18 10:07:45 +0100

    Fixes + Init

--- main/points.js
+++ main/points.js
@@ -11,34 +11,58 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-
-let raiseReverse = document.getElementById('raiseHit');
-let lowerReverse = document.getElementById('lowerHit');
-let raiseHit = document.getElementById('raiseRoll');
-let lowerHit = document.getElementById('lowerRoll');
+let raiseHit = document.getElementById('raiseHit');
+let lowerHit = document.getElementById('lowerHit');
+let raiseRoll = document.getElementById('raiseRoll');
+let lowerRoll = document.getElementById('lowerRoll');
+let raiseBoundary = document.getElementById('raiseBoundary');
+let lowerBoundary = document.getElementById('lowerBoundary');
 
 let int = document.getElementById('score');
 let integer = 000;
 
-raiseRoll.addEventListener('click',function(){
-  integer +=5;
-  int.innerHTML = integer;
-})
-
-lowerRoll.addEventListener('click',function(){
-  integer -=5;
-  int.innerHTML = integer;
-})
-
 raiseHit.addEventListener('click',function(){
-  integer +=10;
+  if (integer < 100) {
+    integer +=10;
+    }
   int.innerHTML = integer;
 })
 
 lowerHit.addEventListener('click',function(){
-  integer -=10;
+  if (integer > 000) {
+    integer -=10;
+    }
   int.innerHTML = integer;
 })
+
+raiseRoll.addEventListener('click',function(){
+  if (integer < 100) {
+    integer +=5;
+    }
+  int.innerHTML = integer;
+})
+
+lowerRoll.addEventListener('click',function(){
+  if (integer > 000) {
+  integer -=5;
+  }
+  int.innerHTML = integer;
+})
+
+raiseBoundary.addEventListener('click',function(){
+  if (integer < 100) {
+    integer +=5;
+    }
+  int.innerHTML = integer;
+})
+
+lowerBoundary.addEventListener('click',function(){
+  if (integer > 000) {
+    integer -=5;
+    }
+  int.innerHTML = integer;
+})
+
 
 
 
